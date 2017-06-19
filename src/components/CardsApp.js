dba0c774e24cdd287dd3f1791cc039467c072300
Alan Vincent <alan.d.vincent@gmail.com>
2017-06-19 15:56:18 -0600

    initial

---
+++ src/components/CardsApp.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class CardsApp extends React.Component {
+  render() {
+      return (
+          <div>
+             test placeholder
+          </div>
+      )
+  }
+}
+
+export default CardsApp;
---
+++ src/components/CardsList.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class CardsList extends React.component {
+  render() {
+    return (
+      <div>
+        list placeholder
+      </div>
+    )
+  }
+}
+
+export default CardsList;
